35d48878aa4d5fa4fb853460e7766feacbec1639
Dzulfikar Adi Putra <dzulfikar.adiputra@gmail.com>
2018-07-01 13:12:44 +0700

    * update app BASE_URL

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
   APP_VERSION: '1.0.9',
-  BASE_URL: 'https://lakonhidup.netlify.com',
-  APP_INFO_JSON: 'https://lakonhidup.netlify.com/static/json/app-info.json',
+  BASE_URL: 'https://lakonhidup.pikarlabs.com/',
+  APP_INFO_JSON: 'https://lakonhidup.pikarlabs.com/static/json/app-info.json',
 };
